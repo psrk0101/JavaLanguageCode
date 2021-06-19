a2415b39fb2a46194efec8a7412b7b81ed8b6848
psrk0101 <psrk0101@naver.com>
2021-06-19 10:53:43 +0900

    BitWiseOpeator

--- src/bitwise/Basic.java
+++ src/bitwise/Basic.java
@@ -2,6 +2,7 @@ package bitwise;
 
 public class Basic {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        BitMoveOperator bmo = new BitMoveOperator();
+
     }
 }
---
+++ src/bitwise/BitMoveOperator.java
@@ -0,0 +1,17 @@
+package bitwise;
+
+class BitMoveOperator {
+
+    /** LeftMove
+     * - Syntax : x << y
+     * - Description
+     *  :
+     * */
+    void leftMoveOperator(){
+
+    }
+
+    void rightMoveOperator(){
+
+    }
+}
