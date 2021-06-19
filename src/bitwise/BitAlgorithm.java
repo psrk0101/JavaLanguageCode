b7bab745dcea011fb326ff7ff83a4f8966a13b38
psrk0101 <psrk0101@naver.com>
2021-06-19 15:12:35 +0900

    BitOperation

---
+++ src/bitwise/BitAlgorithm.java
@@ -0,0 +1,4 @@
+package bitwise;
+
+public class BitAlgorithm {
+}
--- src/bitwise/BitRunner.java
+++ src/bitwise/BitRunner.java
@@ -1,6 +1,6 @@
 package bitwise;
 
-public class Basic {
+public class BasicRunner {
     public static void main(String[] args){
         BitShiftOperator bmo = new BitShiftOperator();
         bmo.leftShiftOperator();
