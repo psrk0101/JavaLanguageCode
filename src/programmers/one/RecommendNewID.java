a15d76086e813e4328c91af0e365bb6ca6facc66
psrk0101 <psrk0101@naver.com>
2021-06-26 15:57:09 +0900

    Programmers Problem Solving

--- src/programmers/one/RecommendNewID.java
+++ src/programmers/one/RecommendNewID.java
@@ -16,6 +16,9 @@ import java.util.regex.Pattern;
  *  Step3) new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
  *  Step4) new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
  *  Step5) new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+ *  Step6) new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+ *         만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표 문자는 제거
+ *  Step7) new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될때까지 반복한다.
 * */
 public class RecommendNewID {
     private String new_id;
