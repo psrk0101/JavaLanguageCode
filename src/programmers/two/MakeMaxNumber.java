617d3a615f29d8cfe841fbd893aff245033ed6ad
psrk0101 <psrk0101@naver.com>
2021-07-06 21:34:09 +0900

    programmers algorithm

---
+++ src/programmers/two/MakeMaxNumber.java
@@ -0,0 +1,29 @@
+package programmers.two;
+
+/** 큰 수 만들기
+ * - 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+ * - param
+ * 1) String number : 숫자의 문자열 형식
+ * 2) int k : 제거할 숫자의 개수
+ *
+ * - Question
+ * : number에서 k개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 만드세요
+ *
+ * - ex
+ * number   k   return
+ * "1924"   2   "94"
+ * "1231234"    3   "3234"
+ * "4177252841" 4   "775841"
+ * */
+public class MakeMaxNumber {
+    public static void main(String[] args){
+
+    }
+
+    public String solution(String number, int k){
+        String answer = "";
+
+
+        return answer;
+    }
+}
