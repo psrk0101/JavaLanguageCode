a22fddae75e3ba421e4dc60d534be4aeb619c645
psrk0101 <psrk0101@naver.com>
2021-08-18 20:46:24 +0900

    programmers second week

--- src/programmers/weekchallenge/CoEvaluation.java
+++ src/programmers/weekchallenge/CoEvaluation.java
@@ -1,5 +1,7 @@
 package programmers.weekchallenge;
 
+import java.util.Arrays;
+
 /** 상호 평가
  *
  * 대학 교수인 당신은, 상호평가를 통하여 학생들이 제출한 과제물에 학점을 부여하려고 합니다.
@@ -18,20 +20,63 @@ package programmers.weekchallenge;
  * */
 public class CoEvaluation {
     public static void main(String[] args){
-
-    }
-
-    private double calculateAvg(int[] score, int selfIndex){
-        return 0;
-    }
-
-    private String calculateGrade(double score){
-        return "A";
+        int[][] test = {{50, 90}, {50, 87}};
+        // int[][] test = {{70,49,90}, {68,50,38}, {73,31,100}};
+        System.out.println(new CoEvaluation().solution(test));
     }
 
     public String solution(int[][] scores){
-        String answer = "";
+        int[] user_score;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < scores.length; i++){
+            user_score = new int[scores[i].length];
+            for(int j = 0; j < scores[i].length; j++){
+                user_score[j] = scores[j][i];
+            }
+            sb.append(getGrade(getAvg(user_score, i)));
+        }
+        return sb.toString();
+    }
 
-        return answer;
+    private double getAvg(int[] scores, int selfIndex){
+        int sum = Arrays.stream(scores).sum();
+        if(checkMinMax(scores, scores[selfIndex]) && checkUnique(scores, scores[selfIndex])){
+            return (sum - scores[selfIndex]) / (scores.length - 1);
+        }
+
+        return sum / scores.length;
+    }
+
+    private boolean checkMinMax(int[] scores, int selfScore){
+        Arrays.sort(scores);
+        if(scores[0] == selfScore || scores[scores.length-1] == selfScore){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkUnique(int[] scores, int selfScore){
+        int cnt=0;
+        for(int score : scores)
+        {
+            if(score == selfScore){
+                cnt++;
+            }
+        }
+        return cnt == 1 ? true : false;
+    }
+
+    private String getGrade(double avg_score){
+        if(avg_score >= 90){
+            return "A";
+        }else if(avg_score >= 80 && avg_score < 90){
+            return "B";
+        }else if(avg_score >= 70 && avg_score < 80){
+            return "C";
+        }else if(avg_score >= 50 && avg_score < 70){
+            return "D";
+        }else{
+            return "F";
+        }
     }
 }
