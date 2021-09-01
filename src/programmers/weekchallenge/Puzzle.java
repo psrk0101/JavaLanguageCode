4dd793bdc6deb8a09140740d032c6b50818a7648
unknown <psrk0101@naver.com>
2021-09-01 21:27:26 +0900

    hello

---
+++ src/programmers/weekchallenge/Puzzle.java
@@ -0,0 +1,12 @@
+package programmers.weekchallenge;
+
+public class Puzzle {
+    public static void main(String[] args){
+
+    }
+
+    public int solution(int[][] game_borad, int[][] table){
+        int answer = -1;
+        return answer;
+    }
+}
