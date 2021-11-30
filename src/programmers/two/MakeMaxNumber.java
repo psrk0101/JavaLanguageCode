637da536df0c17912a5ef01538f2c78c68df5dd4
SangJunPark <psrk0101@naver.com>
2021-11-30 22:53:48 +0900

    Programmers - 큰 수 구하기 - Stack 활용한 LIFO

--- src/programmers/two/MakeMaxNumber.java
+++ src/programmers/two/MakeMaxNumber.java
@@ -1,5 +1,7 @@
 package programmers.two;
 
+import java.util.Stack;
+
 /** 큰 수 만들기
  * - 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
  * - param
@@ -17,13 +19,43 @@ package programmers.two;
  * */
 public class MakeMaxNumber {
     public static void main(String[] args){
-
+        System.out.println(new MakeMaxNumber().solution("4177252841", 4));
     }
 
     public String solution(String number, int k){
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        int index = -1;
 
+        // number.length()-k회 반복
+        for(int i = 0; i < number.length()-k; i++){
+            char maxChar = '0';
+            for(int j = index+1; j <= i+k; j++){
+                if(maxChar < number.charAt(j)){
+                    index = j;
+                    maxChar = number.charAt(j);
+                }
+            }
+            sb.append(maxChar);
+        }
 
-        return answer;
+        return sb.toString();
+    }
+
+    public String bestSolution(String number, int k){
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
     }
 }
